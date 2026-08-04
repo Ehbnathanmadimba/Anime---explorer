96a0559f33fe9ba90d40e001b1e0791953aaa2c9
Ehbnathanmadimba <lungeni.madimba.betukumesu@student.ehb.be>
2026-08-04 13:26:59 +0200

    feat(theme): koppel klik event aan de thema knop

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
+'use strict';
+
 import '../css/style.css';
+
+const themeButton = document.querySelector('#theme-button');
+
+themeButton.addEventListener('click', () => {
+  const isLicht = document.body.classList.toggle('licht');
+  themeButton.textContent = isLicht ? 'Donker thema' : 'Licht thema';
+});
