46b48829f161b0b74e8ed70deb21aa7e299da8e6
Ehbnathanmadimba <lungeni.madimba.betukumesu@student.ehb.be>
2026-08-05 21:00:58 +0200

    feat(main): importeer api module in main.js en test de console log

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import '../css/style.css';
+import { getTopAnime } from './api.js';
 
 const themeButton = document.querySelector('#theme-button');
 
@@ -8,3 +9,10 @@ themeButton.addEventListener('click', () => {
   const isLicht = document.body.classList.toggle('licht');
   themeButton.textContent = isLicht ? 'Donker thema' : 'Licht thema';
 });
+
+const start = async () => {
+  const animeLijst = await getTopAnime();
+  console.log('Opgehaalde anime:', animeLijst);
+};
+
+start();
