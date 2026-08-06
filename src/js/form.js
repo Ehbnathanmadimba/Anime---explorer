aae8e6bc8a61eefcb5abc533f839e0723ec4349f
Ehbnathanmadimba <lungeni.madimba.betukumesu@student.ehb.be>
2026-08-06 19:06:25 +0200

    feat(form): maak form.js aan voor invoercontrole

---
+++ src/js/form.js
@@ -0,0 +1,7 @@
+'use strict';
+
+export const searchForm = document.querySelector('#search-form');
+export const searchInput = document.querySelector('#search-input');
+export const searchError = document.querySelector('#search-error');
+export const filterType = document.querySelector('#filter-type');
+export const sortSelect = document.querySelector('#sort-select');
