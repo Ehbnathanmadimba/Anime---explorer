8a0bc1a446879f3bd568c29ffcff26740e1d7b76
Ehbnathanmadimba <lungeni.madimba.betukumesu@student.ehb.be>
2026-08-06 18:56:56 +0200

    feat(ui): voeg knop toe om te wisselen tussen kaarten en tabel

--- src/js/main.js
+++ src/js/main.js
@@ -2,22 +2,40 @@
 
 import '../css/style.css';
 import { getTopAnime } from './api.js';
-import { renderAnimeCards, setStatus } from './ui.js';
+import { renderAnimeCards, renderAnimeTable, setStatus } from './ui.js';
 
 const themeButton = document.querySelector('#theme-button');
+const viewButton = document.querySelector('#view-button');
+
+let huidigeLijst = [];
+let toonTabel = false;
+
+const toonResultaten = () => {
+  if (toonTabel) {
+    renderAnimeTable(huidigeLijst);
+  } else {
+    renderAnimeCards(huidigeLijst);
+  }
+};
 
 themeButton.addEventListener('click', () => {
   const isLicht = document.body.classList.toggle('licht');
   themeButton.textContent = isLicht ? 'Donker thema' : 'Licht thema';
 });
 
+viewButton.addEventListener('click', () => {
+  toonTabel = !toonTabel;
+  viewButton.textContent = toonTabel ? 'Toon als kaarten' : 'Toon als tabel';
+  toonResultaten();
+});
+
 const start = async () => {
   setStatus('Bezig met laden...');
 
-  const animeLijst = await getTopAnime();
+  huidigeLijst = await getTopAnime();
 
   setStatus('');
-  renderAnimeCards(animeLijst);
+  toonResultaten();
 };
 
 start();
